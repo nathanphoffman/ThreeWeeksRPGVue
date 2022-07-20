20e06f745b4f768875719c0f9b2d932f77e015c5
Nate <natephiliphoffman@gmail.com>
2022-07-20 13:31:34 -0400

    twrpg download comp.
    three weeks components and logo

---
+++ src/components/Download.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    let props = defineProps<{ title?: string, url: string }>();
+</script>
+
+<template>
+    <li><a :href="`/files/${props.url}`">{{title}}</a>
+        
+    </li> 
+    <p>
+        <slot></slot>
+    </p>
+</template>
+
+<style scoped>
+li {
+    margin-top: 10px;
+    list-style: none;
+}
+
+p {
+    border-bottom: 1px solid #eee;
+}
+</style>
---
+++ src/views/DownloadView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Download from './../components/Download.vue';
+</script>
+
+<template>
+  <main>
+    <div>
+        <h2>Downloads</h2>
+        <ul>
+            <Download url="/TWRPG_0_0_2.pdf" title="Three Weeks RPG Beta 0.2.2 Core Ruleset">
+                The original ruleset for Three Weeks RPG, also includes printable DM Sheets along with example player powers and a Character Sheet
+            </Download>
+        </ul>
+    </div>
+  </main>
+</template>
+
+
+<style scoped>
+ul {
+    padding-left: 0px;
+    margin-left: 0px;
+}
+</style>
+
